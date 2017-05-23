c992e45fb4e953dee5faa03711ef2e59cf79d46b
zhaomingli <zhaomingli@juzifenqi.com>
2017-05-23 17:52:54 +0800

    iii

--- common/src/main/java/world.java
+++ common/src/main/java/world.java
@@ -2,7 +2,10 @@ package main.java;
 
 
 public class world {
-    public void  ss(){
+    private static void  ss(){
         System.out.println("sdfsdf");
+    }
+    public static void main(String[] args) {
+        ss();
     }
 }
