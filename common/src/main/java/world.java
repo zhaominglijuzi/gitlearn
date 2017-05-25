06968b462a3cad1790f2eb0e301cdbc69b2c96cf
zhaomingli <zhaomingli@juzifenqi.com>
2017-05-25 16:57:17 +0800

    iii

--- common/src/main/java/world.java
+++ common/src/main/java/world.java
@@ -5,6 +5,9 @@ public class world {
     private static void  ss(){
         System.out.println("sdfsdf");
         System.out.println("999");
+        System.out.println("999");
+        System.out.println("999");
+        System.out.println("999");
     }
     public static void main(String[] args) {
         ss();
